8e41d392171c8bbfb6280713974ef6240153e8ce
Theo-neuromedia <tlecardonnel@neuromedia.io>
2023-03-21 17:53:33 -0400

    Load data

--- src/scripts/viz2.js
+++ src/scripts/viz2.js
@@ -1,17 +1,9 @@
-import d3 from "d3"
 
-/**
- * Defines the contents of the tooltip.
- *
- * @param {object} d The data associated to the hovered element
- * @returns {string} The tooltip contents
- */
-export function getContents (d) {
-  
-  const toolTipContent = 
-  `<div id='tooltip-title'>${d.target.__data__.Player}</div>` +
-  "<br>" +
-  `<div class='tooltip-value'>${d.target.__data__.Count} lines</div>`
-  
-  return toolTipContent
-}
+
+// chargement des données
+d3.csv('./volsQuebec2022.csv').then(function (data, error) {
+  console.log(data)
+
+
+
+})
