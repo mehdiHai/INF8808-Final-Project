73598975aa3eb6af9de56925f336b7f46872328d
Theo-neuromedia <tlecardonnel@neuromedia.io>
2023-03-22 13:51:28 -0400

    Beta version

--- src/scripts/viz2.js
+++ src/scripts/viz2.js
@@ -1,55 +1,42 @@
 import * as d3geo from 'd3-geo';
 
-// chargement des données
-d3.csv('./volsQuebec2022.csv').then(function (data, error) {
-  //console.log(data)
-  var Gen= d3.line()
-  var svg = d3.select('#viz2').append('svg')
-            .attr('height', '500px')
-            .attr('width', '500px');
+d3.queue()
+  .defer(d3.csv, "./airports.csv")
+  .defer(d3.csv, "./flights.csv")
+  .await(ready);
+
+function ready(error, airports, flights) {
+
   var projection = d3.geoProjection(function (x, y) {
     return [x, Math.log(Math.tan(Math.PI / 4 + y / 2))];
   });
-  projection=d3geo.geoNaturalEarth1();
+  projection = d3geo.geoNaturalEarth1();
+
+
+  var airportCode = {}
+  for (const item of airports) {
+    airportCode[item.airport] = projection([item.lon, item.lat]);
+  }
+
+  var svg = d3.select('#viz2')
+    .append('svg')
+    .attr("viewBox", "0 0 1000 1000")
   
-  svg.selectAll('circle').data(data.slice(1,100)).join('circle')
-    .attr("transform", d => `translate(${projection([d.longIn, d.latIn])})`)
-    .attr("r", 3)
-    .style('fill','red')
-  
-    svg.selectAll('line').data(data.slice(1,100)).join('line')
-    //.attr("transform", d => `translate(${projection([d.longIn, d.latIn])})`)
-    //.attr("r", 1)
-    .attr('x1',d => projection([d.longIn, d.latIn])[0])
-    .attr('y1',d => projection([d.longIn, d.latIn])[1])
-    .attr('x2',d => projection([d.longOut, d.latOut])[0])
-    .attr('y2',d => projection([d.longOut, d.latOut])[1])
-    .style('stroke-width',1)
-    .style('stroke','black')
+  svg.selectAll('circle')
+    .data(airports)
+    .join('circle')
+    .attr("transform", d => `translate(${airportCode[d.airport]})`)
+    .attr("r", 1)
+    .style('fill', 'red')
 
-})
-/*
-export function draw (data, color) {
-
-  
-  var d = d3.select('.legend')
-  .selectAll('div')
-  .data(data)
-  .enter()
-  .append('div')
-  .attr('class', 'legend-element')
-
-d.append('div')
-  .attr('class', 'legend-element')
-  .style('width', 15)
-  .style('height', 15)
-  .style('background-color', name => color(name))
-  .style('border', "1px solid black")
-  .style('display', 'inline-block')
-  .style('margin-right', '3px')
-
-d.append('text')
-  .text(name => name);
-
+  svg.selectAll('line')
+    .data(flights)
+    .join('line')
+    .attr('x1', d => airportCode[d.airportIn][0])
+    .attr('y1', d => airportCode[d.airportIn][1])
+    .attr('x2', d => airportCode[d.airportOut][0])
+    .attr('y2', d => airportCode[d.airportOut][1])
+    .style('stroke-width', 1)
+    .style('stroke', d => 'rgba(0, 0, 0,' + d.number / 100 + ')')
+    
 }
-*/
