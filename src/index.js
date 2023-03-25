65a49b7d98da9a3d8eb33d8a7f7c1aec2fda5cfd
SimCena <simon-alexandre.chayer@polymtl.ca>
2023-03-25 13:27:11 -0400

    Affichage des formes brutes dans le graphique de seau

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+import * as buckets from './scripts/viz1.js'
+
+d3.csv('./volsQuebec2022.csv').then(function (data) {
+    const bigCompanies = buckets.getCompaniesFlightCount(data)
+    const heightScale = buckets.createHeightScale(bigCompanies);
+
+    buckets.displayBucketGraph(bigCompanies, 8, heightScale)
+
+})
--- src/scripts/viz1.js
+++ src/scripts/viz1.js
@@ -1,11 +1,7 @@
-d3.csv('./volsQuebec2022.csv').then(function (data) {
-  console.log(data)
+const BUCKET_HEIGHT = 300;
+const BUCKET_WIDTH = 200;
 
-  const bigCompanies = getBigCompanies(data);
-})
-
-
-function getBigCompanies(data) {
+export function getCompaniesFlightCount(data) {
   const bigCompanies = new Map()
   data.forEach((d) => {
     if(!bigCompanies.get(d.opérateur)) {
@@ -17,6 +13,82 @@ function getBigCompanies(data) {
   })
   bigCompanies.delete('NULL')
   bigCompanies.delete('')
-  console.log(bigCompanies);
-  return [...bigCompanies.entries()].sort((a, b) => b[1] - a[1]).slice(0, 5)
+  fixSemanticBugs(bigCompanies)
+  return [...bigCompanies.entries()].sort((a, b) => b[1] - a[1])
+}
+
+function fixSemanticBugs(bigCompanies){
+  bigCompanies.set("AIR CANADA", bigCompanies.get("AIR CANADA ") + bigCompanies.get("AIR CANADA"))
+  bigCompanies.delete("AIR CANADA ")
+}
+
+export function displayBucketGraph(companiesFlightArray, topCompanyNumber, heightScale) {
+  const bottomBucket = [...companiesFlightArray];
+  const topBucket = bottomBucket.splice(0, topCompanyNumber);
+
+  displayTopBucket(topBucket, heightScale);
+  displayBottomBucket(bottomBucket, heightScale);
+ 
+}
+function displayTopBucket(topBucket, heightScale) {
+  d3.select("#top").append('svg')
+  .attr('class', 'topSVG')
+  .attr('height', BUCKET_HEIGHT + 'px')
+  .attr('width', BUCKET_WIDTH + 'px')
+  .selectAll('.topCompany')
+  .data(topBucket)
+  .enter()
+  .append('g')
+  .append('rect')
+  .attr('class', 'topCompany')
+  .attr('y', function(c, index) {
+    let y = BUCKET_HEIGHT;
+    for(let i =0; i <= index; i++){
+      y -= heightScale(topBucket[i][1])
+    }
+    return y;
+  })
+  .attr('fill', function(c, i) {
+    if(i %2 == 0 ){
+      return 'blue'
+    }
+    else {
+      return 'red'
+    }
+  })
+  .attr('height', function(c) { 
+    return heightScale(c[1])})
+  .attr('width', BUCKET_WIDTH)
+}
+
+function displayBottomBucket(bottomBucket, heightScale) {
+  const height = heightScale(d3.sum(bottomBucket, function(c) {return c[1]}))
+  d3.select("#bottom").append('svg')
+    .attr('class', 'bottomSVG')
+    .attr('height', BUCKET_HEIGHT + 'px')
+    .attr('width', BUCKET_WIDTH + 'px')
+    .append('g')
+    .attr('width', BUCKET_WIDTH)
+    .style('text-align', 'center')
+    .attr('class', 'bottomCompany')
+    .append('rect')
+    .attr('y', function(c, index) {
+      return BUCKET_HEIGHT - height;
+    })
+    .attr('fill', "gray")
+    .attr('height', function(c) { 
+      return height})
+    .attr('width', BUCKET_WIDTH)
+  d3.select('.bottomCompany').append('text').attr('y', function() {
+      return BUCKET_HEIGHT - height/2;
+    })
+    .attr('x', function() {return BUCKET_WIDTH / 2})
+    .attr('font-size', '18px')
+    .attr('text-anchor', 'middle')
+    .text('Restes des compagnies')
+}
+
+export function createHeightScale (topCompanies) {
+  const maxHeight = d3.sum(topCompanies, c => c[1])
+  return d3.scaleLinear().domain([d3.min(topCompanies, function(d) {return d[1]}), maxHeight]).range([0, BUCKET_HEIGHT])
 }
