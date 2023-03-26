f52505f78b8cf083cada4b3e462ec8c06a53e271
SimCena <simon-alexandre.chayer@polymtl.ca>
2023-03-26 10:59:52 -0400

    Ajout d'un slider et d'un tooltip fonctionnelle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 'use strict'
 
 import * as buckets from './scripts/viz1.js'
+import * as preprocess from './scripts/preprocess.js'
 
+export let data = [];
 
 d3.csv('./volsQuebec2022.csv').then(function (data) {
-    const bigCompanies = buckets.getCompaniesFlightCount(data)
-    const heightScale = buckets.createHeightScale(bigCompanies);
-
-    buckets.displayBucketGraph(bigCompanies, 5, heightScale)
+    // const bigCompanies = buckets.getCompaniesFlightCount(data)
+    // const heightScale = buckets.createHeightScale(bigCompanies);
+    preprocess.setData(data);
+    buckets.displayBucketGraph(5);
+    buckets.setUpSlider();
 
 })
--- src/scripts/viz1.js
+++ src/scripts/viz1.js
@@ -1,9 +1,37 @@
 import d3Tip from 'd3-tip'
+import * as preprocess from './preprocess.js'
 
 const BUCKET_HEIGHT = 300;
 const BUCKET_WIDTH = 200;
 
-export function getCompaniesFlightCount(data) {
+
+
+export function displayBucketGraph(topCompanyNumber) {
+  const companiesFlightArray = getCompaniesFlightCount(preprocess.getData())
+  const heightScale = createHeightScale(companiesFlightArray);
+  const bottomBucket = [...companiesFlightArray];
+  const topBucket = bottomBucket.splice(0, topCompanyNumber);
+
+  setUpSlider()
+  displayTopBucket(topBucket, heightScale);
+  displayBottomBucket(bottomBucket, heightScale);
+  setTooltips(); 
+}
+
+export function setUpSlider(){
+  var	slider=document.getElementById("slider");
+	var sliderValue=document.getElementById("slider-value");
+	sliderValue.innerHTML=slider.value;
+	slider.onchange=function(){
+		sliderValue.innerHTML=this.value;
+    d3.select('#topSVG').selectAll('*').remove();
+    d3.select('#bottomSVG').selectAll('*').remove();
+    displayBucketGraph(this.value);
+	}
+}
+
+
+function getCompaniesFlightCount(data) {
   const bigCompanies = new Map()
   data.forEach((d) => {
     if(!bigCompanies.get(d.opérateur)) {
@@ -24,21 +52,9 @@ function fixSemanticBugs(bigCompanies){
   bigCompanies.delete("AIR CANADA ")
 }
 
-export function displayBucketGraph(companiesFlightArray, topCompanyNumber, heightScale) {
-  const bottomBucket = [...companiesFlightArray];
-  const topBucket = bottomBucket.splice(0, topCompanyNumber);
 
-  displayTopBucket(topBucket, heightScale);
-  displayBottomBucket(bottomBucket, heightScale);
-  setTooltips();
- 
-}
 function displayTopBucket(topBucket, heightScale) {
-  d3.select("#top").append('svg')
-  .attr('class', 'topSVG')
-  .attr('height', BUCKET_HEIGHT + 'px')
-  .attr('width', BUCKET_WIDTH + 'px')
-  .style('border-radius', '4%')
+  d3.select("#topSVG")
   .selectAll('.topCompany')
   .data(topBucket)
   .enter()
@@ -64,15 +80,18 @@ function displayTopBucket(topBucket, heightScale) {
   .attr('height', function(c) { 
     return heightScale(c[1])})
   .attr('width', BUCKET_WIDTH)
+  .on("mouseover", showTooltipTop )
+  .on("mousemove", moveTooltip )
+  .on("mouseleave", hideTooltip )
 }
 
 function displayBottomBucket(bottomBucket, heightScale) {
   const height = heightScale(d3.sum(bottomBucket, function(c) {return c[1]}))
-  d3.select("#bottom").append('svg')
-    .attr('class', 'bottomSVG')
-    .attr('height', BUCKET_HEIGHT + 'px')
-    .attr('width', BUCKET_WIDTH + 'px')
-    .style('border-radius', '4%')
+  let top5Company = ""
+  for(let i = 0; i < 5; i++){
+    top5Company += (i+1) + '.' + ' ' + bottomBucket[i][0] + ': ' + bottomBucket[i][1] + ' vols <br>'
+  }
+  d3.select("#bottomSVG")
     .append('g')
     .attr('width', BUCKET_WIDTH)
     .style('text-align', 'center')
@@ -85,12 +104,16 @@ function displayBottomBucket(bottomBucket, heightScale) {
     .attr('height', function(c) { 
       return height})
     .attr('width', BUCKET_WIDTH)
+    .on("mouseover", function(m) { return showTooltipBottom(m, top5Company) })
+    .on("mousemove", moveTooltip )
+    .on("mouseleave", hideTooltip )
   d3.select('.bottomCompany').append('text').attr('y', function() {
       return BUCKET_HEIGHT - height/2;
     })
     .attr('x', function() {return BUCKET_WIDTH / 2})
     .attr('font-size', '18px')
     .attr('text-anchor', 'middle')
+    .style('pointer-events', 'none')
     .text('Restes des compagnies')
 }
 
@@ -100,18 +123,49 @@ export function createHeightScale (topCompanies) {
 }
 
 function setTooltips(){
-  const tip = d3Tip().attr('class', 'd3-tip').html(function (d) { return `
-  <label>${d[0]} : ${d[1]}</label>
-  ` })
-  d3.select('.topSVG').call(tip)
-  d3.select('.topSVG')
-    .selectAll('rect')
-    .on('mouseenter', function (t, d) {
-      d3.select(this).attr('fill-opacity', 1.0)
-      tip.show(d, this)
-    })
-    .on('mouseleave', function () {
-      d3.select(this).attr('fill-opacity', 0.7)
-      tip.hide()
-    })
+  d3.select("#viz1")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "black")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+    .style("color", "white")
+    .style('position', 'absolute')
+}
+
+var showTooltipTop = function(m, d) {
+  const tooltip = d3.select('#viz1').select('.tooltip');
+  tooltip
+    .transition()
+    .duration(100)
+  tooltip
+    .style("opacity", 1)
+    .html(d[0] + ": " + d[1])
+    .style("left", (m.x+30) + "px")
+    .style("top", (m.y+30) + "px")
+    .style('width', null)
+
+}
+var showTooltipBottom = function(m, d) {
+  const tooltip = d3.select('#viz1').select('.tooltip');
+
+  tooltip
+    .style("opacity", 1)
+    .html("Prochaines 5 plus grandes compagnies: <br>" + d)
+    .style("left", (m.x+30) + "px")
+    .style("top", (m.y+30) + "px")
+    .style('width', '300px')
+}
+var moveTooltip = function(m) {
+  const tooltip = d3.select('#viz1').select('.tooltip');
+  console.log(m.x);
+  tooltip
+    .style("left", (m.x+30) + "px")
+    .style("top", (m.y+30) + "px")
+}
+var hideTooltip = function() {
+  const tooltip = d3.select('#viz1').select('.tooltip');
+  tooltip
+    .style("opacity", 0)
 }
