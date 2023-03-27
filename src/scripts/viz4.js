7b4be766ea01d0bc98358e9c3f3868b155fba177
SimCena <simon-alexandre.chayer@polymtl.ca>
2023-03-27 15:17:37 -0400

    Gaufre is gaufring

--- src/scripts/viz4.js
+++ src/scripts/viz4.js
@@ -44,4 +44,67 @@ function drawBigWaffle() {
   //   .attr('y', function (d, i) {
   //     return (i % 10) * 500 / 10
   //   });
+  const sequence = (length) => Array.apply(null, {length: length}).map((d, i) => i);
+  const waffleScale = d3.scaleBand()
+    .domain(sequence(10))
+    .range([0, 600])
+    .padding(0.1)
+  const color = d3.scaleOrdinal(d3.schemeTableau10)
+    .domain(sequence(otherCompaniesAircrafts.length))
+  
+  const chartData =   [...otherCompaniesAircrafts.entries()]
+  const whole = true;
+  const width = 1024;
+  const height = 600;
+  const isRect = true;
+
+  const waffles = [];
+  const max = chartData.length; 
+  let index = 0, curr = 1, 
+      accu = Math.round(chartData[0][0]), waffle = [];
+  
+  for (let y = 9; y >= 0; y--)
+    for (let x = 0; x < 10; x ++) {
+      if (curr > accu && index < max) {
+        let r = Math.round(chartData[++index][0]);
+        while(r === 0 && index < max) r = Math.round(chartData[++index][0]);
+        accu += r;
+      }
+      waffle.push({x, y, index});
+      curr++;
+    } 
+  waffles.push(waffle);
+ 
+  console.log(waffles)
+
+  const svg = d3.select('#waffleChart').attr("viewBox", [0, 0, width, height])
+
+  const g = svg
+      .selectAll('.waffle')
+      .data(waffles)
+      .enter()
+      .append("g")
+      .attr("class", "waffle");
+
+  const cellSize = waffleScale.bandwidth();
+  const half = cellSize / 2;
+  const cells = g.append("g")
+    .selectAll('rect')
+    .data(d => d)
+    .join('rect')
+    .attr("fill", d => d.index === -1 ? "#ddd" : color(d.index));
+
+  cells.attr("x", d => waffleScale(d.x))
+    .attr("y", d => whole ?  0 : waffleScale(d.y))
+    .attr("rx", 3).attr("ry", 3)
+    .attr("width", cellSize).attr("height", cellSize)      
+
+  cells.transition()
+    .duration(d => d.y * 100)
+    .ease(d3.easeBounce)
+    .attr(isRect ? "y" : "cy", d => waffleScale(d.y) + (isRect ? 0 : half));
+    svg.transition().delay(550)
+
+  svg.node();
+    
 }
