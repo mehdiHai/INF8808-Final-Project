62191963d728dadecedd20f1113475af1a83ddf3
Theo-neuromedia <tlecardonnel@neuromedia.io>
2023-03-28 16:58:06 -0400

    Change aggregation

--- src/scripts/preprocess.js
+++ src/scripts/preprocess.js
@@ -24,25 +24,14 @@ function getCompaniesFlightCount() {
       topCompanies.set(d.company, topCompanies.get(d.company) + parseFloat(d.number))
     }
   })
-  console.log(topCompanies)
   topCompanies.delete('NULL')
   topCompanies.delete('')
-  fixSemanticBugs(topCompanies) // effectif ?
 
-  let numOthers = topCompanies.get("Others")
-  topCompanies.delete("Others")
+  let numOthers = topCompanies.get("OTHERS")
+  topCompanies.delete("OTHERS")
 
   let sortedCompanies = [...topCompanies.entries()].sort((a, b) => b[1] - a[1])
 
-  sortedCompanies.push(["Others", numOthers])
+  sortedCompanies.push(["OTHERS", numOthers])
   return sortedCompanies
-}
-
-function fixSemanticBugs(topCompanies){
-  topCompanies.set("AIR CANADA", topCompanies.get("AIR CANADA ") + topCompanies.get("AIR CANADA"))
-  topCompanies.delete("AIR CANADA ")
-  topCompanies.set("AIR FRANCE", topCompanies.get("AIR FRANS") + topCompanies.get("AIR FRANCE"))
-  topCompanies.delete("AIR FRANS")
-  topCompanies.set("AIR TRANSAT", topCompanies.get("TRANSAT") + topCompanies.get("AIR TRANSAT"))
-  topCompanies.delete("TRANSAT")
 }
