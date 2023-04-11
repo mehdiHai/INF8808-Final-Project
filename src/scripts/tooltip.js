d8d085ad6c393a2b7803063fad57485bca1f4d9a
Pierre-Emmanuel Rebours <pe.rebours@etu.emse.fr>
2023-04-11 12:39:47 -0400

    show tooltip

--- src/scripts/tooltip.js
+++ src/scripts/tooltip.js
@@ -147,6 +147,7 @@ export default class Tooltip {
             .html(text)
             .style("left", (m.x + 30) + "px")
             .style("top", (m.pageY + 30) + "px")
+            .style("display", "block")
     }
 
 
@@ -173,6 +174,7 @@ export default class Tooltip {
             .html(text)
             .style("left", (m.x + 30) + "px")
             .style("top", (m.pageY + 30) + "px")
+            .style("display", "block")
     }
 
     showTooltipAirport(m, d) {
@@ -181,6 +183,7 @@ export default class Tooltip {
             .html(d[0] + "<br>" + d[2] + " - " + d[1] + "<br>"+ d[3] + " vols")
             .style("left", ((m.x > window.innerWidth * 0.5) ? (m.x - 90) : (m.x + 30)) + "px")
             .style("top", (m.pageY + 30) + "px")
+            .style("display", "block")
     }
 
     showTooltipAircrafts(m, d) {
